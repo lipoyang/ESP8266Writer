eb10b019420760daffedc7658a6bf62d00ed93c9
Bizan Nishimura <lipoyang1453@gmail.com>
2020-12-16 10:04:52 +0900

    add attach() method

--- ESP8266Writer.cpp
+++ ESP8266Writer.cpp
@@ -1,11 +1,29 @@
-#include <Arduino.h>
 #include "ESP8266Writer.h"
 
 // Global obcject
 ESP8266WriterClass ESP8266Writer;
 
 // GR-ROSE's Serial6 is ESP8266 I/F 
-static HardwareSerial* serial = &Serial6;
+#ifdef GRROSE
+static HardwareSerial* s_serial = &Serial6;
+static int s_pin_esp_en   = PIN_ESP_EN;
+static int s_pin_esp_io0  = PIN_ESP_IO0;
+static int s_pin_esp_io15 = PIN_ESP_IO15;
+#else
+static HardwareSerial* s_serial;
+static int s_pin_esp_en;
+static int s_pin_esp_io0;
+static int s_pin_esp_io15;
+#endif
+
+// attach serial port (not for GR-ROSE's onboard ESP8266)
+void ESP8266WriterClass::attach(HardwareSerial &serial, int en, int io0, int io15)
+{
+    s_serial = &serial;
+    s_pin_esp_en   = en;
+    s_pin_esp_io0  = io0;
+    s_pin_esp_io15 = io15;
+}
 
 // begin writer mode if mode_pin is LOW
 void ESP8266WriterClass::begin(int mode_pin, int baud)
@@ -34,31 +52,31 @@ void ESP8266WriterClass::begin(int mode_pin, int baud)
     
     // setup serial ports
     Serial.begin(9600);
-    serial->begin(baud);
+    s_serial->begin(baud);
     
     // turn ESP8266 to flash program mode
-    pinMode(PIN_ESP_EN, OUTPUT);
-    pinMode(PIN_ESP_IO0, OUTPUT);
-    pinMode(PIN_ESP_IO15, OUTPUT);
+    pinMode(s_pin_esp_en,   OUTPUT);
+    pinMode(s_pin_esp_io0,  OUTPUT);
+    pinMode(s_pin_esp_io15, OUTPUT);
     
-    digitalWrite(PIN_ESP_IO0, LOW); // LOW -> UART Download Mode
-    digitalWrite(PIN_ESP_IO15, LOW);
-    digitalWrite(PIN_ESP_EN, LOW);
+    digitalWrite(s_pin_esp_io0,  LOW); // LOW -> UART Download Mode
+    digitalWrite(s_pin_esp_io15, LOW);
+    digitalWrite(s_pin_esp_en,   LOW);
     delay(100);
-    digitalWrite(PIN_ESP_EN, HIGH);
+    digitalWrite(s_pin_esp_en,   HIGH);
     
     // serial port bridge
     while(true)
     {
         if(Serial.available()){
-            serial->write(Serial.read());
+            s_serial->write(Serial.read());
             
             digitalWrite(PIN_LED1, HIGH);
         }else{
             digitalWrite(PIN_LED1, LOW);
         }
-        if(serial->available()){
-            Serial.write(serial->read());
+        if(s_serial->available()){
+            Serial.write(s_serial->read());
             
             digitalWrite(PIN_LED2, HIGH);
         }else{
@@ -96,31 +114,31 @@ void ESP8266WriterClass::beginTest(int mode_pin, int baud)
     
     // setup serial ports
     Serial.begin(9600);
-    serial->begin(baud);
+    s_serial->begin(baud);
     
     // turn ESP8266 to flash program mode
-    pinMode(PIN_ESP_EN, OUTPUT);
-    pinMode(PIN_ESP_IO0, OUTPUT);
-    pinMode(PIN_ESP_IO15, OUTPUT);
+    pinMode(s_pin_esp_en,   OUTPUT);
+    pinMode(s_pin_esp_io0,  OUTPUT);
+    pinMode(s_pin_esp_io15, OUTPUT);
     
-    digitalWrite(PIN_ESP_IO0, HIGH); // HIGH -> Flash Run Mode
-    digitalWrite(PIN_ESP_IO15, LOW);
-    digitalWrite(PIN_ESP_EN, LOW);
+    digitalWrite(s_pin_esp_io0,  HIGH); // HIGH -> Flash Run Mode
+    digitalWrite(s_pin_esp_io15, LOW);
+    digitalWrite(s_pin_esp_en, LOW);
     delay(100);
-    digitalWrite(PIN_ESP_EN, HIGH);
+    digitalWrite(s_pin_esp_en, HIGH);
     
     // serial port bridge
     while(true)
     {
         if(Serial.available()){
-            serial->write(Serial.read());
+            s_serial->write(Serial.read());
             
             digitalWrite(PIN_LED1, HIGH);
         }else{
             digitalWrite(PIN_LED1, LOW);
         }
-        if(serial->available()){
-            Serial.write(serial->read());
+        if(s_serial->available()){
+            Serial.write(s_serial->read());
             
             digitalWrite(PIN_LED2, HIGH);
         }else{
--- ESP8266Writer.h
+++ ESP8266Writer.h
@@ -1,12 +1,17 @@
 #ifndef __ESP8266_WRITER__
 #define __ESP8266_WRITER__
 
+#include <Arduino.h>
+
 #define NOT_USE_MODE_PIN	-1
 
 // ESP8266 Flash writer
 class ESP8266WriterClass
 {
 public:
+    // attach serial port (not for GR-ROSE's onboard ESP8266)
+    void attach(HardwareSerial &serial, int en, int io0, int io15);
+
     // begin writer mode if mode_pin is LOW
     void begin(int mode_pin = NOT_USE_MODE_PIN, int baud = 115200);
 
